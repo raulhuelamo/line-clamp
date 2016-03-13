fc4caf2384f984ce749af81e248a90eb13854b50
yuanqing <desk@yuanqing.sg>
2016-03-13 21:26:34 +0800

    Tweak

--- example/index.js
+++ example/index.js
@@ -1,23 +1,10 @@
 import lineClamp from '../src';
 
-lineClamp(document.querySelector('.en'), {
-  lineCount: 3,
-  lineHeight: 20
-});
+const elements = [].slice.call(document.querySelectorAll('.line-clamp'))
 
-lineClamp(document.querySelector('.en-native'), {
-  lineCount: 3,
-  lineHeight: 20,
-  shouldUseNativeLineClamp: true
-});
-
-lineClamp(document.querySelector('.cn'), {
-  lineCount: 3,
-  lineHeight: 20
-});
-
-lineClamp(document.querySelector('.cn-native'), {
-  lineCount: 3,
-  lineHeight: 20,
-  shouldUseNativeLineClamp: true
+elements.forEach((element) => {
+  lineClamp({
+    lineCount: 3,
+    lineHeight: 20
+  })(element);
 });
--- src/index.js
+++ src/index.js
@@ -1,83 +1,74 @@
 const ELLIPSIS = '\u2026';
-const SPLIT_REGEX = /(?=\s)/;
-const TRIM_TRAILING_REGEX = /[\s.,]+$/;
+const WHITESPACE_REGEX = /(?=\s)/;
+const TRAILING_WHITESPACE_REGEX = /\s+$/;
 
-export default (element, options = {}) => {
+// Truncate the text of `element` such that it does not exceed the
+// `maximumHeight`. Return `true` if we need to truncate by character, else
+// return `false`.
+function truncateByWord(element, maximumHeight) {
+  const innerHTML = element.innerHTML;
 
-  // Options.
-  const lineCount = options.lineCount || 1;
-  const lineHeight = options.lineHeight;
-  const shouldUseNativeLineClamp = options.shouldUseNativeLineClamp;
-  const ellipsisCharacter = options.ellipsisCharacter || ELLIPSIS;
+  // Split the content of `element` by the space character.
+  const splitCharacter = ' ';
+  let chunks = innerHTML.split(WHITESPACE_REGEX);
 
-  const cssText = 'overflow:hidden;overflow-wrap:break-word;word-wrap:break-word;';
-
-  // Use native `line-clamp` if desired, and if it is supported.
-  const elementStyle = element.style;
-  if (shouldUseNativeLineClamp && typeof elementStyle.webkitLineClamp !== 'undefined') {
-    elementStyle.cssText = `${cssText}display:-webkit-box;text-overflow:ellipsis;-webkit-box-orient:vertical;-webkit-line-clamp:${lineCount};`;
-    return;
+  // The text does not contain whitespace; we need to attempt to truncate
+  // by character.
+  if (chunks.length === 1) {
+    return true;
   }
-  elementStyle.cssText = cssText;
 
-  // Truncate the value of `element` such that it does not exceed the
-  // `maximumHeight`. Return `true` if we need to truncate by character, else
-  // return `false`.
-  function truncateByWord(element, maximumHeight) {
-    const innerHTML = element.innerHTML;
+  // Loop over the chunks, and try to fit more chunks into the `element`.
+  let i = -1;
+  const length = chunks.length;
+  let newInnerHTML = '';
+  while (++i < length) {
+    newInnerHTML += chunks[i];
+    element.innerHTML = newInnerHTML;
 
-    // Split the content of `element` by the space character.
-    const splitCharacter = ' ';
-    let chunks = innerHTML.split(SPLIT_REGEX);
-
-    // The `element` value does not contain whitespace; we need to attempt to
-    // truncate by character.
-    if (chunks.length === 1) {
+    // If the new height now exceeds the `maximumHeight` (where it did not
+    // in the previous iteration), we know that we are at most one line
+    // over the optimal text length.
+    if (element.offsetHeight >= maximumHeight) {
       return true;
     }
+  }
 
-    // Initialise the value to the first chunk.
-    element.innerHTML = chunks[0];
+  return false;
+}
 
-    // Loop over the remaining chunks, and try to fit `result` plus the
-    // `currentChunk` into the `element`
-    let i = 0;
-    const length = chunks.length;
-    while (++i < length) {
-      element.innerHTML += chunks[i];
+// Append `ELLPISIS` to `element`, trimming off trailing characters in
+// `element` such that `element` will not exceed the `maximumHeight`.
+function truncateByCharacter(element, maximumHeight, ellipsisCharacter) {
+  const innerHTML = element.innerHTML;
+  let length = innerHTML.length;
 
-      // If the new height now exceeds the `maximumHeight` (where it did not
-      // in the previous iteration), we know that we are at most one line
-      // over the optimal length.
-      if (element.offsetHeight > maximumHeight) {
-        return true
-      }
+  // In each iteration, we trim off one trailing character . Also trim
+  // off any trailing punctuation before appending the `ellipsisCharacter`.
+  while (length > 0) {
+    element.innerHTML = innerHTML.substring(0, length).replace(TRAILING_WHITESPACE_REGEX, '') + ellipsisCharacter;
+    if (element.offsetHeight <= maximumHeight) {
+      return;
     }
+    length--;
+  }
+}
 
-    return false;
+function lineClamp(maximumHeight, ellipsisCharacter, element) {
+  // Exit if text does not overflow the `element`.
+  if (element.scrollHeight <= maximumHeight) {
+    return;
   }
 
-  // Append `ELLPISIS` to `element`, trimming off trailing characters in
-  // `element` such that `element` will not exceed the `maximumHeight`.
-  function truncateByCharacter(element, maximumHeight, ellipsisCharacter) {
-    const innerHTML = element.innerHTML;
+  // Else truncate by word, then truncate by character.
+  truncateByWord(element, maximumHeight);
+  truncateByCharacter(element, maximumHeight, ellipsisCharacter);
+}
 
-    let length = innerHTML.length;
-    while (length > 0) {
-      // In each iteration, we trim off one more trailing character . Also trim
-      // off any trailing punctuation before appending the `ellipsisCharacter`.
-      element.innerHTML = innerHTML.substring(0, length).replace(TRIM_TRAILING_REGEX, '')
-        + ellipsisCharacter;
-      if (element.offsetHeight <= maximumHeight) {
-        return;
-      }
-      length--;
-    }
-  }
-
-  const maximumHeight = lineHeight * lineCount;
-  if (truncateByWord(element, maximumHeight)) {
-    truncateByCharacter(element, maximumHeight, ellipsisCharacter);
-  }
-
+export default (options = {}) => {
+  return lineClamp.bind(
+    null,
+    options.lineCount * options.lineHeight,
+    options.ellipsisCharacter || ELLIPSIS
+  );
 };
