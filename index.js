5102e91153d676521698fa1d0c5382d54de42f41
Yuan Qing <desk@yuanqing.sg>
2018-02-20 00:34:09 +0800

    Rewrite

--- index.js
+++ index.js
@@ -1,75 +1,116 @@
-var ELLIPSIS = '\u2026'
-var WHITESPACE_REGEX = /(?=\s)/
-var TRAILING_WHITESPACE_REGEX = /\s+$/
+function truncateTextNode (
+  textNode,
+  rootElement,
+  maximumHeight,
+  ellipsisCharacter
+) {
+  truncateTextNodeByWord(
+    textNode,
+    rootElement,
+    maximumHeight,
+    ellipsisCharacter
+  )
+  return truncateTextNodeByCharacter(
+    textNode,
+    rootElement,
+    maximumHeight,
+    ellipsisCharacter
+  )
+}
 
-// Truncate the text of `element` such that it does not exceed the
-// `maximumHeight`. Return `true` if we need to truncate by character, else
-// return `false`.
-function truncateByWord (element, maximumHeight) {
-  var innerHTML = element.innerHTML
-
-  // Split the text of `element` by whitespace.
-  var chunks = innerHTML.split(WHITESPACE_REGEX)
-
-  // The text does not contain whitespace; we need to attempt to truncate
-  // by character.
-  if (chunks.length === 1) {
-    return true
+function truncateTextNodeByWord (textNode, rootElement, maximumHeight) {
+  var lastIndexOfWhitespace
+  var textContent = textNode.textContent
+  while (textContent.length > 1) {
+    lastIndexOfWhitespace = textContent.lastIndexOf(' ')
+    if (lastIndexOfWhitespace == -1) {
+      return
+    }
+    textNode.textContent = textContent.substring(0, lastIndexOfWhitespace)
+    if (rootElement.scrollHeight <= maximumHeight) {
+      textNode.textContent = textContent
+      return
+    }
+    textContent = textNode.textContent
   }
+}
 
-  // Loop over the chunks, and try to fit more chunks into the `element`.
-  var i = -1
-  var length = chunks.length
-  var newInnerHTML = ''
-  while (++i < length) {
-    newInnerHTML += chunks[i]
-    element.innerHTML = newInnerHTML
-
-    // If the new height now exceeds the `maximumHeight` (where it did not
-    // in the previous iteration), we know that we are at most one line
-    // over the optimal text length.
-    if (element.offsetHeight > maximumHeight) {
+var TRAILING_WHITESPACE_AND_PUNCTUATION_REGEX = /[ .,;!?'‘’“”\-–—]+$/
+function truncateTextNodeByCharacter (
+  textNode,
+  rootElement,
+  maximumHeight,
+  ellipsisCharacter
+) {
+  var textContent = textNode.textContent
+  var length = textContent.length
+  while (length > 1) {
+    // Trim off one trailing character and any trailing punctuation and whitespace.
+    textContent = textContent
+      .substring(0, length - 1)
+      .replace(TRAILING_WHITESPACE_AND_PUNCTUATION_REGEX, '')
+    length = textContent.length
+    textNode.textContent = textContent + ellipsisCharacter
+    if (rootElement.scrollHeight <= maximumHeight) {
       return true
     }
   }
-
   return false
 }
 
-// Append `ellipsisCharacter` to `element`, trimming off trailing characters
-// in `element` such that `element` will not exceed the `maximumHeight`.
-function truncateByCharacter (element, maximumHeight, ellipsisCharacter) {
-  var innerHTML = element.innerHTML
-  var length = innerHTML.length
-
-  // In each iteration, we trim off one trailing character . Also trim
-  // off any trailing punctuation before appending the `ellipsisCharacter`.
-  while (length > 0) {
-    element.innerHTML =
-      innerHTML.substring(0, length).replace(TRAILING_WHITESPACE_REGEX, '') +
-      ellipsisCharacter
-    if (element.offsetHeight <= maximumHeight) {
-      return
+function truncateElementNode (
+  element,
+  rootElement,
+  maximumHeight,
+  ellipsisCharacter
+) {
+  var childNodes = element.childNodes
+  var i = childNodes.length - 1
+  while (i > -1) {
+    var childNode = childNodes[i--]
+    var nodeType = childNode.nodeType
+    if (
+      (nodeType == 1 &&
+        truncateElementNode(
+          childNode,
+          rootElement,
+          maximumHeight,
+          ellipsisCharacter
+        )) ||
+      (nodeType == 3 &&
+        truncateTextNode(
+          childNode,
+          rootElement,
+          maximumHeight,
+          ellipsisCharacter
+        ))
+    ) {
+      return true
     }
-    length--
+    element.removeChild(childNode)
   }
+  return false
 }
 
-module.exports = function (element, options) {
-  // Read the `line-height` of `element`, and use it to compute the height of
-  // `element` required to fit the given `lineCount`.
-  var lineHeight = parseInt(window.getComputedStyle(element).lineHeight, 10)
-  var maximumHeight = options.lineCount * lineHeight
+var ELLIPSIS_CHARACTER = '\u2026'
 
-  // Exit if text does not overflow the `element`.
-  if (element.scrollHeight <= maximumHeight) {
+module.exports = function (rootElement, lineCount, options) {
+  rootElement.style.cssText +=
+    'overflow:hidden;overflow-wrap:break-word;word-wrap:break-word'
+
+  var maximumHeight =
+    (lineCount || 1) *
+    parseInt(window.getComputedStyle(rootElement).lineHeight, 10)
+
+  // Exit if text does not overflow `rootElement`.
+  if (rootElement.scrollHeight <= maximumHeight) {
     return
   }
 
-  truncateByWord(element, maximumHeight)
-  truncateByCharacter(
-    element,
+  truncateElementNode(
+    rootElement,
+    rootElement,
     maximumHeight,
-    options.ellipsisCharacter || ELLIPSIS
+    (options && options.ellipsis) || ELLIPSIS_CHARACTER
   )
 }
